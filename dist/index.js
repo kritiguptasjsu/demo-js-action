c113b752b3f5f43691c9944105c1c6e1d13872ad
Kriti Gupta <kritig4@gmail.com>
2024-06-21 15:30:16 -0500

    Bug fixes

--- dist/index.js
+++ dist/index.js
@@ -31083,12 +31083,12 @@ const github = __nccwpck_require__(3007);
 
 try{
     const yourName = core.getInput('name-of-you');
-    console.log('Hello ${yourName}!');
+    console.log(`Hello ${yourName}!`);
     const time = (new Date()).toTimeString();
     core.setOutput("time", time);
     
     const payload = JSON.stringify(github.context.payload, undefined, 2);
-    console.log('The event payload: ${payload}');
+    console.log(`The event payload: ${payload}`);
 }catch(error){
     core.setFailed(error.message);
 }
--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const github = require('@actions/github');
 
 try{
     const yourName = core.getInput('name-of-you');
-    console.log('Hello ${yourName}!');
+    console.log(`Hello ${yourName}!`);
     const time = (new Date()).toTimeString();
     core.setOutput("time", time);
     
     const payload = JSON.stringify(github.context.payload, undefined, 2);
-    console.log('The event payload: ${payload}');
+    console.log(`The event payload: ${payload}`);
 }catch(error){
     core.setFailed(error.message);
 }
